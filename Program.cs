7531dc00132ac96f08b36b3bd255e55e24eeca21
Hugo <tricoire@et.esiea.fr>
2024-02-07 14:55:43 +0100

    Fin API

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy  =>
                       {
-                          policy.WithOrigins("http://localhost:3000");
+                          policy.WithOrigins("http://localhost:3000","http://localhost:3030");
                       });
 });
 
